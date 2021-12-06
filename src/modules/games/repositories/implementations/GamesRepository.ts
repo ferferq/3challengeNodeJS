17999496bdf0e28bdbed19c3e41cb686a03d3f2f
fmartins <fernandoalvesq@gmail.com>
2021-12-06 15:09:33 -0300

    3 desafio do ignite - concluido

---
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -0,0 +1,39 @@
+import { getRepository, Repository } from 'typeorm';
+
+import { User } from '../../../users/entities/User';
+import { Game } from '../../entities/Game';
+
+import { IGamesRepository } from '../IGamesRepository';
+
+export class GamesRepository implements IGamesRepository {
+  private repository: Repository<Game>;
+
+  constructor() {
+    this.repository = getRepository(Game);
+  }
+
+  async findByTitleContaining(param: string): Promise<Game[]> {
+    return  await this.repository
+      .createQueryBuilder()
+      .where(`LOWER(title) LIKE LOWER(:title)`, {title: `%${param}%`})
+      .getMany();
+    //   // Complete usando query builder
+  }
+
+  async countAllGames(): Promise<[{ count: string }]> {
+    return this.repository.query('SELECT COUNT(id) FROM games'); // Complete usando raw query
+  }
+
+  async findUsersByGameId(id: string): Promise<User[]> { 
+     const data = await this.repository
+       .createQueryBuilder("games")
+       .leftJoinAndSelect("games.users", "users")
+       .where('games.id = :id', { id })
+       .getOne();
+
+      if (data?.users)
+      return data?.users;
+      else
+      return [];
+  }
+}
---
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -0,0 +1,40 @@
+import { getRepository, Repository } from 'typeorm';
+
+import { IFindUserWithGamesDTO, IFindUserByFullNameDTO } from '../../dtos';
+import { User } from '../../entities/User';
+import { IUsersRepository } from '../IUsersRepository';
+
+export class UsersRepository implements IUsersRepository {
+  private repository: Repository<User>;
+
+  constructor() {
+    this.repository = getRepository(User);
+  }
+
+  async findUserWithGamesById({
+    user_id,
+  }: IFindUserWithGamesDTO): Promise<User> {
+    const user = await this.repository.findOne(user_id, { relations: ["games"] });
+
+    if (!user) {
+      throw new Error("User id not found");
+    }
+
+    return user;
+  }
+
+  async findAllUsersOrderedByFirstName(): Promise<User[]> {
+    return this.repository.query(
+      'SELECT * FROM "users" ORDER BY "first_name" ASC, "id" DESC'
+    ); // Complete usando raw query
+  }
+
+  async findUserByFullName({
+    first_name,
+    last_name,
+  }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
+    return this.repository.query(
+      `SELECT * FROM users WHERE LOWER(first_name) LIKE LOWER('${first_name}') AND LOWER(last_name) LIKE LOWER('${last_name}')`
+    ); // Complete usando raw query
+  }
+}
